3efa762fbcd7b447a1cb786332206bb2fdb5a5e5
Emily Deans <emilyldeans@gmail.com>
2017-09-14 09:25:56 -0400

    Added initial actions and set permissions on projects controller.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,20 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.all
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def show
+    @project = Project.fin(params[:id])
+  end
+
+private
+
+  def project_params
+    params.require(:project).permit(:name, :description, developers_attributes: [:id, :name, :email], time_entries_attributes: [:entry])
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170913214129) do
+ActiveRecord::Schema.define(version: 20170914124058) do
 
   create_table "developers", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20170913214129) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "time_entries", force: :cascade do |t|
+    t.float "entry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
