8b2a4776622173dca2a03af6fb33bc54e4187172
Emily Deans <emilyldeans@gmail.com>
2017-09-14 08:58:28 -0400

    Created final TimeEntry model and corresponding controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'time_entries/index'
+
+  get 'times/index'
+
+  get 'projects/index'
+
+  get 'developers/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
