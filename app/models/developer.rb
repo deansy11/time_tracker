f8a9c9ebb55797f7edc7f69362869ec2821d9841
Emily Deans <emilyldeans@gmail.com>
2017-09-14 10:43:45 -0400

    Created model associations and validation for developer unique email.

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,2 +1,17 @@
 class Developer < ApplicationRecord
+  belongs_to :project
+  has_many :time_entries
+
+  accepts_nested_attributes_for :project, :reject_if => :all_blank
+
+  validates :email,
+    uniqueness: true,
+    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+
+private
+
+  def downcase_email
+    email.downcase if email
+  end
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ApplicationRecord
+  has_many :developers
+
+  accepts_nested_attributes_for :developers, :reject_if => :all_blank
 end
